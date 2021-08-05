becfb1ff44ac631c90a19e9ce6740ca668a3ea86
Surya Pratap Singh <suryatomar303@gmail.com>
2021-08-05 23:41:10 +0530

    Calculator version 1.0

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let runningTotal=0;
+let buffer = "0";
+let previousOperator;
+
+document.querySelector(`.calc-buttons`).addEventListener("click",function(event) {
+    buttonClick(event.target.value);
+});
+function(buttonClick(value){
+    if(isNaN(parseInt(value))){
+        
+    } else{
+
+    }
+})
